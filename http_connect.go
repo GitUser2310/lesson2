1021c3989ee86ad9057e43c1cba1202fd19fa7fc
GitUser2310 <gituser@smlsat.ru>
2019-09-23 22:21:20 +0300

    added pattern

--- http_connect.go
+++ http_connect.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	tmp1 := template.New("main")
+	tmp1, _ = tmp1.Parse(`
+				<div style="display: inline-block; background-color: orange; border: 1px solid #aaa; border-radius: 3px; padding: 30px; margin: 20px;">
+				<pre>{{.}}</pre>
+				</div>`)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
 
@@ -15,13 +23,16 @@ func main() {
 		resp, err := c.Get("http://artii.herokuapp.com/make?text=" + path)
 		if err != nil {
 			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Error"))
+			return
 		}
 		defer resp.Body.Close()
 
 		body, _ := ioutil.ReadAll(resp.Body)
 
-		w.WriteHeader(http.StatusOK)
-		w.Write(body)
+		// return body in pattern
+		tmp1.Execute(w, string(body))
 	})
 
 	http.ListenAndServe(":8081", nil)
