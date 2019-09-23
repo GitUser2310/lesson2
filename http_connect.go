0619dcde725aafd3393c9f068a4208ad30fb4c93
GitUser2310 <gituser@smlsat.ru>
2019-09-23 21:42:30 +0300

    lesson2 http-connect witj herokuapp.com

---
+++ http_connect.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+
+		c := http.Client{}
+
+		resp, err := c.Get("http://artii.herokuapp.com/make?text=" + path)
+		if err != nil {
+			log.Println(err)
+		}
+		defer resp.Body.Close()
+
+		body, _ := ioutil.ReadAll(resp.Body)
+
+		w.WriteHeader(200)
+		w.Write(body)
+	})
+
+	http.ListenAndServe(":8081", nil)
+}
